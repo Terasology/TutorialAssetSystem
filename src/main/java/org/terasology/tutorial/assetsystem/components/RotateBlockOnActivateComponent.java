f8cb5e9b62e6de530d2ab095471112735f384e26
DarkWeird <foxflameless@gmail.com>
2021-08-30 11:29:29 +0300

    chore(tutorial): migrate tutorial modules with engine package movement

--- src/main/java/org/terasology/tutorial/assetsystem/components/RotateBlockOnActivateComponent.java
+++ src/main/java/org/terasology/tutorial/assetsystem/components/RotateBlockOnActivateComponent.java
@@ -1,6 +1,6 @@
 package org.terasology.tutorial.assetsystem.components;
 
-import org.terasology.entitySystem.Component;
+import org.terasology.engine.entitySystem.Component;
 
 /**
  * Marker component for a block which will be rotated if activated
