74b212756aec04188b67fda6b28c10dd756c38cb
Nail Khanipov <foxflameless@gmail.com>
2021-09-03 01:20:27 +0300

    chore: Update tutorials to lastest engine (#15)
    * chore(tutorial): migrate tutorial modules with engine package movement
    
    * chore(tutorial): migrate components to Gestaltv7

--- src/main/java/org/terasology/tutorial/assetsystem/components/RotateBlockOnActivateComponent.java
+++ src/main/java/org/terasology/tutorial/assetsystem/components/RotateBlockOnActivateComponent.java
@@ -1,10 +1,10 @@
 package org.terasology.tutorial.assetsystem.components;
 
-import org.terasology.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
 
 /**
  * Marker component for a block which will be rotated if activated
  */
-public class RotateBlockOnActivateComponent implements Component {
+public class RotateBlockOnActivateComponent extends EmptyComponent<RotateBlockOnActivateComponent> {
 
 }
--- src/main/java/org/terasology/tutorial/assetsystem/systems/RotateBlockOnActivationSystem.java
+++ src/main/java/org/terasology/tutorial/assetsystem/systems/RotateBlockOnActivationSystem.java
@@ -4,20 +4,20 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterMode;
-import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.logic.common.ActivateEvent;
-import org.terasology.math.Side;
-import org.terasology.registry.In;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterMode;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.logic.common.ActivateEvent;
+import org.terasology.engine.math.Side;
+import org.terasology.engine.registry.In;
 import org.terasology.tutorial.assetsystem.components.RotateBlockOnActivateComponent;
-import org.terasology.utilities.random.FastRandom;
-import org.terasology.utilities.random.Random;
-import org.terasology.world.WorldProvider;
-import org.terasology.world.block.Block;
-import org.terasology.world.block.BlockComponent;
+import org.terasology.engine.utilities.random.FastRandom;
+import org.terasology.engine.utilities.random.Random;
+import org.terasology.engine.world.WorldProvider;
+import org.terasology.engine.world.block.Block;
+import org.terasology.engine.world.block.BlockComponent;
 
 import com.google.common.collect.Lists;
 
@@ -34,7 +34,7 @@ public class RotateBlockOnActivationSystem extends BaseComponentSystem {
     @ReceiveEvent(components = {RotateBlockOnActivateComponent.class, BlockComponent.class})
     public void onActivate(ActivateEvent event, EntityRef entity) {
         BlockComponent blockComponent = entity.getComponent(BlockComponent.class);
-        Block block = blockComponent.block;
+        Block block = blockComponent.getBlock();
         List<Block> blocks = Lists.newArrayList(block.getBlockFamily().getBlocks());
         //-1 because we omit the block with the same rotation as the current one
         int index = random.nextInt(blocks.size() - 1);
